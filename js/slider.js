30c5ae70c68097c2fe9fa350703fe50ce64106a3
Higor Soares <higorsbarreto@hotmail.com>
2024-02-16 14:42:38 -0300

    botao add/prev

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,14 @@ var sliderContainer = document.querySelector('.jl-slider-container');
 var sliderList = document.querySelector('.jl-slider-list');
 var sliderItem = document.querySelectorAll('.jl-slider-item');
 var sliderListWidth = null;
+const sliderTotalItems = sliderItem.length;
+var prevItem = document.querySelector('.jl-item-prev');
+var nextItem = document.querySelector('.jl-item-next');
+var sliderPos = 0;
+var curretnSlide = document.querySelector('.jl-current-slide');
+var totalSlide = document.querySelector('.jl-total-slide');
+var currentCounter = 1;
+
 
 //Capturando larguras individuais
 var containerWidth = sliderContainer.parentElement.offsetWidth;
@@ -22,14 +30,9 @@ for (var p = 0; p < sliderItem.length; p++) {
 sliderList.style.width = sliderListWidth + 'px';
 
 
-//Fazendo Animaçao do Slider onClick
-
- var prevItem = document.querySelector('.jl-item-prev');
- var nextItem = document.querySelector('.jl-item-next');
- var sliderPos = 0;
-
 //Handlers
-//Next Slider Animação
+
+//Next Slide Animação
 var nextSliderAnim = function () {
     var lastItem = sliderListWidth - containerWidth;
     if ((-1*(sliderPos) === lastItem)) return;
@@ -40,7 +43,7 @@ var nextSliderAnim = function () {
         translateX: sliderPos,
       });
 }
-//Prev Slider Animação
+//Prev Slide Animação
 var prevSliderAnim = function () {
     if (sliderPos === 0) {
         return;
@@ -53,19 +56,45 @@ var prevSliderAnim = function () {
       });
     };
 
-//Troca Counter
+//Counter Formater
+var counterFormatter = function (i) {
+    if(i < 10) {
+        return '0' + i;
+    } else {
+        return i;
+    }
+}
 
-//Troca Active Navigator
+
+//Counter Add
+var counterAdd = function () {
+    if(currentCounter >= 0 && currentCounter < sliderTotalItems){
+        currentCounter++
+        curretnSlide.innerHTML = counterFormatter(currentCounter);
+   
+    }
+ }
+
+//Counter Remove
+ var counterRemove = function () {
+    if(currentCounter > 1 && currentCounter <= sliderTotalItems){
+        currentCounter--;
+        curretnSlide.innerHTML = counterFormatter(currentCounter);
+    }
+ }
 
 
-
+//Actions
+totalSlide.innerHTML = counterFormatter(sliderTotalItems);
 
  nextItem.addEventListener('click', function () {
    nextSliderAnim();
+   counterAdd();   
  });
 
  prevItem.addEventListener('click', function () {
-    prevSliderAnim();   
+    prevSliderAnim();  
+    counterRemove();   
 });
     
 
